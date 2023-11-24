316b5bef1dc3e225ac718464e69a6ec37f6654eb
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2023-11-24 15:20:03 +0400

    Ok

--- HomeWork_4/shop_app/src/components/My-ProductDetails.vue
+++ HomeWork_4/shop_app/src/components/My-ProductDetails.vue
@@ -10,13 +10,17 @@
   <div>
     <H1>Магазин выпечки</H1>
     <div class="box">
-      <div class="product" v-for="product in productList" :key="product.id">
+      <div class="product">
         <h1>Товар</h1>
-        <h2>{{ product.name }}</h2>
-        <h3 v-if="product.available">Available</h3>
+        <h2>"{{ productName }}"</h2>
+        <h3 v-if="available">Available</h3>
         <h3 v-else>Out of stock</h3>
-        <h3>{{ formattedPrice(product) }}</h3>
-        <button @click="currency === '$' ? currency = 'RUR' : currency = '$' ">Переключатель валюты $/RUR</button>
+        <h3>{{ formattedPrice }}</h3>
+        <button class="curToggle"
+          @click="currency === '$' ? (currency = 'RUR') : (currency = '$')"
+        >
+          Переключатель валюты $/RUR
+        </button>
       </div>
     </div>
   </div>
@@ -28,33 +32,15 @@ export default {
 
   data() {
     return {
-      currency: '$',
-      price: 0,
-      productList: [
-        {
-          name: "Cake",
-          price: 12,
-          available: true,
-          formattedPrice: "",
-        },
-        {
-          name: "Bread",
-          price: 25,
-          available: false,
-          formattedPrice: "",
-        },
-        {
-          name: "Milk",
-          price: 35,
-          available: true,
-          formattedPrice: "",
-        },
-      ],
+      currency: "$",
+      productName: "Cake Napoleon Shokoladniy",
+      productPrice: 12,
+      productAvailable: true,
     };
   },
-  methods: {
-      formattedPrice(product) {
-      return this.currency + product.price;
+  computed: {
+    formattedPrice() {
+      return this.currency + this.productPrice;
     },
   },
 };
@@ -71,6 +57,11 @@ h1 {
 }
 .product {
   background-color: aquamarine;
-  width: 200px;
+  width: 300px;
+}
+.curToggle{
+  color: beige;
+  background-color: rebeccapurple;
+  height: 40px;
 }
 </style>
